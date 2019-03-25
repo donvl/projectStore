0af3e28a5287d477f7f7f51899fe404111fc8378
Donvl <valerijdoktor@gmail.com>
2019-03-25 16:02:35 +0200

    Init commit

---
+++ src/main/java/com/projectStore/projectStore/entity/Description.java
@@ -0,0 +1,22 @@
+package com.projectStore.projectStore.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * Created by 111 on 25.03.2019.
+ */
+@Data
+@Entity
+@Table(name = "description")
+public class Description {
+    @Id
+    @Column(name = "description_id")
+    @SequenceGenerator(name = "description_seq", sequenceName = "description_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "description_seq")
+    private int descriptionId;
+
+    @Column(name = "description_goods")
+    private String descriptionGoods;
+}
---
+++ src/main/java/com/projectStore/projectStore/entity/GoodsType.java
@@ -0,0 +1,22 @@
+package com.projectStore.projectStore.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+/**
+ * Created by 111 on 25.03.2019.
+ */
+@Data
+@Entity
+@Table(name = "goods_type")
+public class GoodsType {
+    @Id
+    @Column(name = "goods_type_id")
+    @SequenceGenerator(name = "goods_type_seq", sequenceName = "goods_type_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "goods_type_seq")
+    private int goodsTypeId;
+
+    @Column(name = "type_name")
+    private String typeName;
+}
