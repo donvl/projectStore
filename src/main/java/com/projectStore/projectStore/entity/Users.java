e1be012b6013a60b571a5a72959947171bfc4be3
lohachovV <lohachov.v@gmail.com>
2019-03-26 16:12:07 +0200

    First commit

---
+++ src/main/java/com/projectStore/projectStore/entity/Users.java
@@ -0,0 +1,19 @@
+package com.projectStore.projectStore.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "users_table")
+
+public class Users {
+
+
+
+
+
+}
