275c58757f837ed791d6dfb961dfc10515d11f9e
Donvl <valerijdoktor@gmail.com>
2019-06-25 09:45:37 +0300

    user page and user controller

--- src/main/java/com/projectStore/projectStore/controller/HomeController.java
+++ src/main/java/com/projectStore/projectStore/controller/HomeController.java
@@ -1,7 +1,10 @@
 package com.projectStore.projectStore.controller;
 
+import com.projectStore.projectStore.service.UserServise;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
@@ -9,15 +12,25 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
+    @Autowired
+    UserServise userServise;
+
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("key", "Главная страница");
+        model.put("textHome", "Главная страница");
         return "home";
     }
+
     @GetMapping("test")
     public String test(Map<String, Object> model) {
         model.put("key", "Страница админа");
         return "test";
     }
 
+    @GetMapping("users")
+    public String users(Map<String, Object> model) {
+
+        model.put("userList", userServise.findAll());
+        return "users";
+    }
 }
---
+++ src/main/java/com/projectStore/projectStore/controller/UserController.java
@@ -0,0 +1,57 @@
+package com.projectStore.projectStore.controller;
+
+import com.projectStore.projectStore.entity.User;
+import com.projectStore.projectStore.service.UserServise;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@Controller
+public class UserController {
+    @Autowired
+    UserServise userServise;
+
+    @RequestMapping(value = "addUser", method = RequestMethod.POST)
+    public String addUser(@RequestParam(value = "login") String login,
+                          @RequestParam(value = "userName") String userName,
+                          @RequestParam(value = "mobile") String mobile,
+                          Map<String, Object> model) {
+        if (mobile.length() < 10 || mobile.length() > 14) {
+            model.put("error", "телефон должен быть от 10 до 14 символов");
+            return "error";
+        }
+        User user = new User();
+        user.setUserLogin(login);
+        user.setUserName(userName);
+        user.setMobile(mobile);
+        userServise.save(user);
+        return "redirect:/users";
+    }
+
+    @RequestMapping(value = "/deleteUser/{userId}", method = RequestMethod.GET)
+    public String deleteUser(@PathVariable("userId") Long id) {
+        userServise.deleteById(id);
+        return "redirect:/users";
+    }
+
+    @RequestMapping(value = "editUser", method = RequestMethod.POST)
+    public String editUser(@RequestParam(value = "login") String login,
+                           @RequestParam(value = "userName") String userName,
+                           @RequestParam(value = "mobile") String mobile,
+                           @RequestParam(value = "userId") Long userId,
+                           Map<String, Object> model) {
+        if (mobile.length() < 10 || mobile.length() > 14) {
+            model.put("error", "телефон должен быть от 10 до 14 символов");
+            return "error";
+        }
+        User user = userServise.finById(userId);
+        user.setUserLogin(login);
+        user.setUserName(userName);
+        user.setMobile(mobile);
+        userServise.upDate(user);
+        return "redirect:/users";
+    }
+}
